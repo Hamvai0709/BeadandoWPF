6a1e313eb9774418b7f7a9a2f6ec2ef0b32472f8
Hamvai <Hamvai@DESKTOP-P6SM9N1>
2023-05-30 20:33:25 +0200

    kommentek

--- BeadandoWPF/MainWindow.xaml.cs
+++ BeadandoWPF/MainWindow.xaml.cs
@@ -15,15 +15,14 @@ using System.Windows.Shapes;
 
 namespace BeadandoWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    // Hamvai Soma - M72A20
+// Festő/rajzoló felület létrehozása WPF segítségével
     public partial class MainWindow : Window
     {
-        private int Atmero = (int)Vastagsag.vekony;
-        private Brush Ecsetszin = Brushes.Black;
-        private bool festes = false;
-        private enum Vastagsag
+        private int Atmero = (int)Vastagsag.vekony; // átmérő létrehozása, alapértelmezés szerint vékony az ecset vastagsága (5p)
+        private Brush Ecsetszin = Brushes.Black; // ecset szín, alapértelmezés szerint fekete
+        private bool festes = false;    // festés változó true/false -> éppen lenyomva van e az egér azaz festünk/rajzolunk e vagy sem
+        private enum Vastagsag //vastagsagok megadasa
         {
             vekony=5,
             kozepes=10,
@@ -36,7 +35,7 @@ namespace BeadandoWPF
             InitializeComponent();
         }
 
-        private void KilepClick(object sender, RoutedEventArgs e)
+        private void KilepClick(object sender, RoutedEventArgs e) // kilépés messagebox-al megerősítéssel
         {
             MessageBoxResult result = MessageBox.Show("Biztosan ki akar lépni?", "Megerősítés", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
             if (result == MessageBoxResult.Yes)
@@ -45,7 +44,7 @@ namespace BeadandoWPF
             }
         }
 
-        private void ecset(Brush szin,Point position)
+        private void ecset(Brush szin,Point position) // ecset létrehozása ellipse-l, kitöltés, méret, pozíció beállítása
         {
             Ellipse newellipse = new Ellipse();
             newellipse.Fill = szin;
@@ -56,52 +55,47 @@ namespace BeadandoWPF
             Vaszon.Children.Add(newellipse);
         }
 
-        private void PirosClick(object sender, RoutedEventArgs e)
+        private void PirosClick(object sender, RoutedEventArgs e) // ecset színének változtatása pirosra
         {
-            if (true)
-            {
                 Ecsetszin = Brushes.Red;
-            }
         }
 
-        private void ZoldClick(object sender, RoutedEventArgs e)
+        private void ZoldClick(object sender, RoutedEventArgs e) // ecset színének változtatása zöldre
         {
             Ecsetszin = Brushes.Green;
         }
 
-        private void KekClick(object sender, RoutedEventArgs e)
+        private void KekClick(object sender, RoutedEventArgs e) // ecset színének változtatása kékre
         {
             Ecsetszin = Brushes.Blue;
         }
 
-        private void VekonyClick(object sender, RoutedEventArgs e)
+        private void VekonyClick(object sender, RoutedEventArgs e) // ecset átmérőjének változtatása vékonyra
         {
-            if (true)
-            {
+
                 Atmero = (int)Vastagsag.vekony;
-            }
         }
 
-        private void KozepesClick(object sender, RoutedEventArgs e)
+        private void KozepesClick(object sender, RoutedEventArgs e) // ecset átmérőjének változtatása közepesre
         {
             Atmero = (int)Vastagsag.kozepes;
         }
 
-        private void VastagClick(object sender, RoutedEventArgs e)
+        private void VastagClick(object sender, RoutedEventArgs e) // ecset átmérőjének változtatása vastagra
         {
             Atmero = (int)Vastagsag.vastag;
         }
 
-        private void Vaszon_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Vaszon_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) // az egér lenyomásakor festünk/rajzolunk
         {
             festes = true;
         }
 
-        private void Vaszon_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void Vaszon_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) // a bal click felengedése után abbahagyjuk a rajzolást/festést
         {
             festes = false;
         }
-        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        private void Canvas_MouseMove(object sender, MouseEventArgs e) // egér mozgatása
         {
             if (festes)
             {
@@ -111,19 +105,24 @@ namespace BeadandoWPF
 
         }
 
-        private void FeherClick(object sender, RoutedEventArgs e)
+        private void FeherClick(object sender, RoutedEventArgs e) // háttérszín fehérre
         {
             Vaszon.Background= new SolidColorBrush(Colors.White);
         }
 
-        private void FeketeClick(object sender, RoutedEventArgs e)
+        private void FeketeClick(object sender, RoutedEventArgs e) // háttérszín feketére
         {
             Vaszon.Background = new SolidColorBrush(Colors.Black);
         }
 
-        private void UjClick(object sender, RoutedEventArgs e)
+        private void UjClick(object sender, RoutedEventArgs e) // vászon törlése/ új létrehozása megerősítéssel
         {
-            Vaszon.Children.Clear();
+            MessageBoxResult result = MessageBox.Show("Új létrehozása/jelenlegi törlése?", "Megerősítés", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+            if (result == MessageBoxResult.Yes)
+            {
+                Vaszon.Children.Clear();
+            }
+
         }
     }
 }
